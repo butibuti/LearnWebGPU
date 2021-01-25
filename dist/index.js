152803ffded4b3e5122520eb19585db9bccbd901
butibuti <butikouki@gmail.com>
2021-01-25 22:23:39 +0900

    fix index.ts

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname+"/../", 'public')))
-  .set('views', path.join(__dirname+"/../", 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  //.get('/cool', (req, res) => res.send(cool())) // routeを追加
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+app.use(express_1.default.static(path.join(__dirname + "/../", 'public')));
+app.set('views', path.join(__dirname + "/../", 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+const router = express_1.default.Router();
+app.use(router);
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-function hello(name: string): string {
-    return `Hello, ${name}!`;
-  }
-  
-  console.log(hello("TypeScript"));
+import express from 'express'
+
+const app: express.Express = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.static(path.join(__dirname+"/../", 'public')));
+app.set('views', path.join(__dirname+"/../", 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const router: express.Router = express.Router();
+router.get('/', (req, res) => res.render('pages/index'));
+app.use(router);
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
