71e58633774b75b34e845fd90d529f4a7991f9a9
butibuti <butikouki@gmail.com>
2021-03-27 23:32:29 +0900

    add live2D

---
+++ src/live2D/main.ts
@@ -0,0 +1,40 @@
+/**
+ * Copyright(c) Live2D Inc. All rights reserved.
+ *
+ * Use of this source code is governed by the Live2D Open Software license
+ * that can be found at https://www.live2d.com/eula/live2d-open-software-license-agreement_en.html.
+ */
+
+import { LAppDelegate } from './lappdelegate';
+import{ LAppLive2DManager} from "./lapplive2dmanager"
+import * as LAppDefine from './lappdefine';
+
+/**
+ * ブラウザロード後の処理
+ */
+window.onload = (): void => {
+  // create the application instance
+  var modelName=(document.getElementById("modelName") as HTMLScriptElement).text;
+  LAppDelegate.CreateInstance([modelName]);
+
+
+  if (LAppDelegate.getInstance().initialize() == false) {
+    return;
+  }
+
+  LAppDelegate.getInstance().run();
+};
+
+/**
+ * 終了時の処理
+ */
+window.onbeforeunload = (): void => LAppDelegate.releaseInstance();
+
+/**
+ * Process when changing screen size.
+ */
+window.onresize = () => {
+  if (LAppDefine.CanvasSize === 'auto') {
+    LAppDelegate.getInstance().onResize();
+  }
+};
---
+++ webpack.live2D.dev.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'production',
+  target: ['web', 'es5'],
+  entry: './src/live2D/main.ts',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  watch: true,
+
+  output: {
+    path: __dirname + '/public/source',
+    filename: 'index_live2D.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      '@framework': path.resolve(__dirname, './src/live2D/Framework')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  devtool: 'inline-source-map'
+}
